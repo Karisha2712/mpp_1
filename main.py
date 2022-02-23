2402dbaf6a120dfc2b34daff11fe5b763954cb5c
Karina Radyuk <radyuk.karina@bk.ru>
2022-02-23 16:25:17 +0300

    feat: adding task to db

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
+from datetime import date
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo_list.db"
@@ -7,24 +8,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['POST', 'GET'])
-@app.route('/todo', methods=['POST', 'GET'])
-def main_page():
-    if request.method == 'POST':
-        text = request.form['task-text']
-        date = request.form['task-date']
-    else:
-        return render_template("index.html")
-
-
-if __name__ == "__main__":
-    app.run()
-
-
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     task_text = db.Column(db.Text, nullable=False)
-    date = db.Column(db.Date, nullable=False)
+    date = db.Column(db.Date, default=date.today())
     status_id = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
@@ -32,9 +19,29 @@ class Task(db.Model):
 
 
 class File(db.Model):
-    id = db.Column(db.Integer, primary_key=True, auto_increment=True)
+    id = db.Column(db.Integer, primary_key=True)
     task_id = db.Column(db.Integer, nullable=False)
     file_name = db.Column(db.String(40), nullable=False)
 
     def __repr__(self):
         return '<File %r>' % self.id
+
+
+@app.route('/', methods=['POST', 'GET'])
+@app.route('/todo', methods=['POST', 'GET'])
+def main_page():
+    if request.method == 'POST':
+        text = request.form['task-text']
+        task = Task(task_text=text, status_id=1)
+        try:
+            db.session.add(task)
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error"
+    else:
+        return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
